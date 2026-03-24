0b0717c76415abdeee89e4b2358c456684f49d44
Maeen Mohammed Abdullah Al-dairy <maeen.mohammedaldeiry@gmail.com>
2026-03-25 02:25:46 +0300

    Change base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // هذا السطر ضروري جداً لأن اسم المستودع الخاص بك ليس الجذر
-  base: '/Maeensoftware-/',
+  base: '/',
 });
